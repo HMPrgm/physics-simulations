92cc234a72a3ce1b44c3f0bc732ddd5dd948adcb
MSI Laptop <henrymunprof@gmail.com>
2024-07-17 14:40:05 -0400

    arrows

--- bodiesInSpace.py
+++ bodiesInSpace.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 from difeqs import universalGravitation, kinematics
 
@@ -12,20 +13,20 @@ dt = .01
 
 m1 = 10
 x1 = (200,300)
-v1 = (0,70.71)
+v1 = (0,70.7106781187)
 r1 = 10
 color1=(255,0,0)
 
 m2 = 1000
 x2=(400,300)
 v2 = (0,0)
-r2 = 10
+r2 = 50
 color2 = (0,0,255)
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-
+xs = []
 
 
 run = True
@@ -39,8 +40,11 @@ while run:
     if collision:
         continue
     screen.fill((0,0,0))
+    # xs.append(x1)
 
-
+    # for i in xs:
+    #     pygame.draw.circle(screen,color1,i, 0.6)
+        
 
 
     pygame.draw.circle(screen,color1,x1, r1)
@@ -57,16 +61,20 @@ while run:
     x1,v1 = kinematics(x1,v1,a1,dt)
     x2,v2 = kinematics(x2,v2,a2,dt)
     
-    pygame.draw.line(screen,color1,(x1),(5*v1[0]+x1[0],5*v1[1]+x1[1]))
-    pygame.draw.line(screen,color1,(x2),(5*v2[0]+x2[0],5*v2[1]+x2[1]))
+    line_Mult = 2
+    width = 2
+    pygame.draw.line(screen,color1,(x1),(line_Mult*v1[0]+x1[0],line_Mult*v1[1]+x1[1]), width=width)
+    # pygame.draw.line(screen,color1,(x2),(line_Mult*v2[0]+x2[0],line_Mult*v2[1]+x2[1]))
     
-    pygame.draw.line(screen,(255,255,0),(x1),(5*a1[0]+x1[0],5*a1[1]+x1[1]))
-    pygame.draw.line(screen,(255,255,0),(x2),(5*a2[0]+x2[0],5*a2[1]+x2[1]))
+    pygame.draw.line(screen,(255,255,0),(x1),(line_Mult*a1[0]+x1[0],line_Mult*a1[1]+x1[1]), width=width)
+    # pygame.draw.line(screen,(255,255,0),(x2),(line_Mult*a2[0]+x2[0],line_Mult*a2[1]+x2[1]))
     
-    xDistance = abs(x2[0]-x1[0])
-    yDistance = abs(x2[1]-x1[1])
-    print (xDistance,yDistance)
-    if (xDistance<(r1+r2)/2 and yDistance<(r1+r2)/2):
+    
+    prominenceVMag = 100
+    xDistance = (x2[0]-x1[0])
+    yDistance = (x2[1]-x1[1])
+    pygame.draw.line(screen,(255,max(0,min(math.dist(x1,x2),255)),0),x2,(x2[0] + prominenceVMag*(-xDistance)/(math.dist(x1,x2)),x2[1] + prominenceVMag*-yDistance//(math.dist(x1,x2))), width=width*2)
+    if (abs(xDistance)<(r1+r2)/2 and abs(yDistance)<(r1+r2)/2):
         print("COLLISION")
         collision = True
     x2 = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
