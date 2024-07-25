4bc91713b9bcdae78d95a5142bbe1130110cf830
MSI Laptop <henrymunprof@gmail.com>
2024-07-25 11:46:24 -0400

    more Euler Approximation

--- eulerApprox.py
+++ eulerApprox.py
@@ -1,17 +1,18 @@
-
 def yPrime(x,y):
-    return 2 * x * y
-x = 1
+    return x**2+y**2
+x = 0
 y = 1
-h = .1
-inc = 5
+h = .05
+inc = 10
+
 
 
 
 def f(n):
     return "{:.4f}".format(n)
 print('   x  |   y  |   y\'  ')
-for i in range(inc):
+for i in range(inc+1):
     print(f'{f(x)}|{f(y)}|{f(yPrime(x,y))}')
-    x+= h;y+= h*yPrime(x,y)
+    y+= h*yPrime(x,y)
+    x+= h
 
